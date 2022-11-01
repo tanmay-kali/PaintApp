b21efdd32a7a7cca137d2d18283589a1f18fba29
tanmay-kali <tanmaykejriwal28@gmail.com>
2022-11-01 15:17:31 -0500

    save left

--- app/src/main/java/edu/tcu/tanmaykejriwal/paint/MainActivity.kt
+++ app/src/main/java/edu/tcu/tanmaykejriwal/paint/MainActivity.kt
@@ -1,18 +1,32 @@
 package edu.tcu.tanmaykejriwal.paint
 import android.app.Dialog
+import android.content.ContentValues
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.drawToBitmap
 import androidx.core.view.get
 import androidx.core.view.iterator
+import com.bumptech.glide.Glide
 import edu.tcu.tanmaykejriwal.paint.DrawingView
+import java.io.File
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +35,10 @@ class MainActivity : AppCompatActivity() {
         var pallet = findViewById<LinearLayout>(R.id.pallete_ll)
         var board = findViewById<DrawingView>(R.id.board)
         var undo = findViewById<ImageView>(R.id.undo)
+        var gallery = findViewById<ImageView>(R.id.gallery)
         var selected=pallet[0]
+        var location=""
+        var save = findViewById<ImageView>(R.id.save)
         for(iv in pallet){
             iv.setOnClickListener {
                 board.SetPathColor((iv.background as ColorDrawable).color)
@@ -59,8 +76,53 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            // Callback is invoked after the user selects a media item or closes the
+            // photo picker.
+            var background = findViewById<ImageView>(R.id.background_img)
+            if (uri != null) {
+                location= uri.toString()
+                //background.setImageURI(Uri.parse(uri.toString()));
+                Glide.with(this)
+                    .load(location)
+                    .into(background)
+                Log.d("PhotoPicker", "Selected URI: $uri")
+            } else {
+                Log.d("PhotoPicker", "No media selected")
+            }
+        }
+
+        gallery.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+
+        //will take time
+        var background = findViewById<ImageView>(R.id.background_img)
+        if(background.drawable==null){
+            background.setBackgroundColor(Color.WHITE)
+        }
+
+        val bitmap = findViewById<FrameLayout>(R.id.frame).drawToBitmap()
+        val values = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, System.currentTimeMillis().toString().substring(2,11)+".jpeg")
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM)
+        }
+
+        //external means outsideof yor app
+        var uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)
+        uri?.let {
+            contentResolver.openOutputStream(it).use {it_->
+                //can take time
+                bitmap.compress(Bitmap.CompressFormat.JPEG,90,it_)
+            }
+        }
+
+
 
     }
+
+
 }
 
 
